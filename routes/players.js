b5cd57eb84d044a8610884ef59fe182cccaa8ffb
Wang Wayne <wangwaynesg@gmail.com>
2022-02-06 23:07:18 +0800

    message

--- routes/players.js
+++ routes/players.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPlayers, createPlayer, getPlayer, deletePlayer, updatePlayer } from "../controllers/Players.js";
+import { getPlayers, createPlayer, getPlayer, deletePlayer, updatePlayer } from "../controllers/players.js";
 
 const router = express.Router();
 
